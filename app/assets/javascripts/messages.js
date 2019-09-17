5d9ad6c9fb054d30180ccbc546766001c8a8293b
Jim-1227 <hajime.minamimoto@gmail.com>
2019-09-17 21:46:31 +0900

    edit message.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,15 @@
-// $(function() {
-//   $(".new_message").on('submit', function(e) {
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     // console.log(this);
-//   })
-// })
+$(function() {
+  $('.new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = location.pathname;
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+})
