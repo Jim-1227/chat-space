024e795e48d53e77e474cf093809d8daf1ce7cb4
Jim-1227 <hajime.minamimoto@gmail.com>
2019-09-18 21:18:07 +0900

    edit message.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,43 @@
 $(function() {
+  function buildHTML(data) {
+    if (data.image) {
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${data.name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${data.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message___content">
+                        ${data.content}
+                      </p>
+                      <img class="lower-message__image" src="${data.image}" alt="${data.name}">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${data.name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${data.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message___content">
+                        ${data.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +49,17 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      // $('.messages').append(buildHTML(data));
+      $('.messages').append(buildHTML(data));
+      $('.form__message').val('');
+      $('.form__submit').attr('disabled', false);
+      console.log($('.message:last').position().top);
+      $('.messages').delay(10).animate({ scrollTop: $('.message:last').position().top }, 1500);
+    })
+    .fail(function(data) {
+      alert("失敗しました");
     })
   })
 })
